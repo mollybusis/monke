79cc32c668043e9059c53ad86ba9a978add8fbda
mollybusis <molly.busis@wustl.edu>
2024-04-16 00:02:30 -0500

    adele

--- frontend/src/Input.js
+++ frontend/src/Input.js
@@ -4,24 +4,13 @@ import './Input.css';
 const Input = () => {
     const debugging_mode = false
     const url = debugging_mode === true ? "http://localhost:3001/" : "https://carefully-certain-swift.ngrok-free.app/"
-    const handleFormSubmit = async (event) => {
+    const handleFormSubmit = (event) => {
         event.preventDefault()
         const answer = event.target.inputbox.value
         console.log("you answered", answer)
-       
-      
-              try {
-                const resultOfAPICall = await axios.post(url  + "addResponse", {
-                    sleepThing: answer
-                })
-
-                console.log(resultOfAPICall)
-                window.location.reload() // forces reload
-              }
-              catch (error) {
-                console.log(error)
-              }
-            
+        axios.post(url  + "addResponse", {
+            sleepThing: answer
+        }).then(result => window.location.reload()).catch(e => console.log(e))
 
     }
     return (<>
